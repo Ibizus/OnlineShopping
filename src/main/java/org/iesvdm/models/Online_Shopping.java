0bbe2914a8d8d2ba8da3d4af102a6424b5574aca
Hector Lopez <hector.ldz@gmail.com>
2023-09-29 00:51:44 +0200

    clases terminadas, faltan metodos

--- src/main/java/org/iesvdm/models/Online_Shopping.java
+++ src/main/java/org/iesvdm/models/Online_Shopping.java
@@ -1,14 +1,53 @@
 package org.iesvdm.models;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Online_Shopping {
 
-    // ATTRIBUTES:
+    /*
+    Crea los siguientes tests:
 
+    Crear varios Web User, Customer, Account.
+    Crear varios Product.
+    Añadir varios Product a ShoppingCart de Web User y Account existente.
+    Crear un Order y Payment asociado a un ShoppingCart existente.
+     */
+
+    // ATTRIBUTES:
+    private HashSet<Customer> customers;
+    private HashSet<WebUser> webUsers;
+    private ArrayList<Product> products;
 
     // CONSTRUCTOR:
-
+    public Online_Shopping() {
+    }
 
     // METHODS:
 
 
+    // GETTERS & SETTERS:
+    public HashSet<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(HashSet<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public HashSet<WebUser> getWebUsers() {
+        return webUsers;
+    }
+
+    public void setWebUsers(HashSet<WebUser> webUsers) {
+        this.webUsers = webUsers;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
 }
--- src/main/java/org/iesvdm/models/Order.java
+++ src/main/java/org/iesvdm/models/Order.java
@@ -1,14 +1,91 @@
 package org.iesvdm.models;
 
+import org.iesvdm.utils.Address;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+
+enum OrderStatus {New, Hold, Shipped, Delivered, Closed};
+
 public class Order {
 
     // ATTRIBUTES:
-
+    private String id;
+    private LocalDate ordered;
+    private LocalDate shipped;
+    private Address ship_to;
+    private OrderStatus status;
+    private BigDecimal total;
+    private Account account;
+    private LinkedHashSet<Payment> payments;
+    private LinkedHashSet<LineItem> lines;
 
     // CONSTRUCTOR:
 
+    public Order(String id, LocalDate ordered, Address ship_to, OrderStatus status, BigDecimal total, Account account) {
+        this.id = id;
+        this.ordered = ordered;
+        this.ship_to = ship_to;
+        this.status = status;
+        this.total = total;
+        this.account = account;
+    }
 
     // METHODS:
 
 
+    // SETTERS:
+    public void setOrdered(LocalDate ordered) {
+        this.ordered = ordered;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setPayments(LinkedHashSet<Payment> payments) {
+        this.payments = payments;
+    }
+
+    public void setLines(LinkedHashSet<LineItem> lines) {
+        this.lines = lines;
+    }
+
+    // GETTERS:
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getOrdered() {
+        return ordered;
+    }
+
+    public LocalDate getShipped() {
+        return shipped;
+    }
+
+    public Address getShip_to() {
+        return ship_to;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public LinkedHashSet<Payment> getPayments() {
+        return payments;
+    }
+
+    public LinkedHashSet<LineItem> getLines() {
+        return lines;
+    }
 }
